e4844ec9a56a3109bb9e4068a6f8da8ac01f561d
khjoony <khjoony4u@gmail.com>
2019-07-27 10:56:52 +0900

    validation form

--- stock/forms.py
+++ stock/forms.py
@@ -1,4 +1,13 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 class SearchForm(forms.Form):
-    word = forms.CharField(label='Search Word')
+    company = forms.CharField(label='Search Word')
+
+    def clean_company(self):
+        data = self.cleaned_data['company']
+
+        # Check if a search word is string
+        return data
+
+
